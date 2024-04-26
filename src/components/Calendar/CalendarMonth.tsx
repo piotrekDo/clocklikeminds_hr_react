d4d945145c8dd67115ebf52f48814b09040b91eb
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2024-04-26 07:21:30 +0200

    employee calendar dates fix

--- src/components/Calendar/CalendarMonth.tsx
+++ src/components/Calendar/CalendarMonth.tsx
@@ -12,6 +12,7 @@ import {
   PopoverHeader,
   PopoverTrigger,
   Text,
+  useFormControlStyles,
 } from '@chakra-ui/react';
 import { FaRegCalendarCheck } from 'react-icons/fa6';
 import { PtoRequestFormatted } from '../../model/Pto';
@@ -28,8 +29,9 @@ export const CalendarMonth = ({ month, holidays, daysOff }: Props) => {
   const days = new Date(month.getFullYear(), month.getMonth() + 1, 0).getDate();
   const currentMonthDays: Date[] = Array.from({ length: days }, (_, index) => {
     const date = new Date(month.getFullYear(), month.getMonth(), index + 1);
-    const offset = new Date().getTimezoneOffset();
-    date.setMinutes(date.getMinutes() - offset);
+    // const offset = new Date().getTimezoneOffset();
+    // date.setMinutes(date.getMinutes() - offset);
+    date.setHours(0)
     return date;
   });
 
@@ -44,7 +46,9 @@ export const CalendarMonth = ({ month, holidays, daysOff }: Props) => {
           const today = new Date();
           const isHoliday: string | undefined = holidays.get(`${day.getMonth()}${day.getDate()}`);
           const isDayOff = daysOff.filter(testedPto => {
-            return day.getTime() >= testedPto.ptoStart.getTime() && day.getTime() <= testedPto.ptoEnd.getTime();
+            // testedPto.ptoStart.setHours(0)
+            // testedPto.ptoEnd.setHours(0)
+            return (day.getTime() >= testedPto.ptoStart.getTime() && day.getTime() <= testedPto.ptoEnd.getTime());
           })[0];
           const isToday =
             day.getFullYear() === today.getFullYear() &&
--- src/hooks/usePtoRequestsForSelectedyear.ts
+++ src/hooks/usePtoRequestsForSelectedyear.ts
@@ -5,15 +5,25 @@ import { getPtoRequestsForSelectedYear } from '../service/PtoHttpService';
 const usePtoRequestsForSelectedYear = (userId: number, year: number) => {
   return useQuery<PtoRequestFormatted[], Error>({
     queryKey: ['ptoReqYear', userId, year],
-    queryFn: () => getPtoRequestsForSelectedYear(year, userId).request
-    .then(res => res.map(pto => ({
-        ...pto,
-        requestDateTime: new Date(pto.requestDateTime),
-        ptoStart: new Date(pto.ptoStart),
-        ptoEnd: new Date(pto.ptoEnd)
-    }))),
+    queryFn: () =>
+      getPtoRequestsForSelectedYear(year, userId).request.then(res =>
+        res.map(pto => {
+          const ptoStart =  new Date(pto.ptoStart);
+          ptoStart.setHours(0, 0, 0, 0);
+          const ptoEnd = new Date(pto.ptoEnd);
+          ptoEnd.setHours(0, 0, 0, 0);
+          console.log(ptoStart)
+          console.log(ptoEnd)
+          return {
+            ...pto,
+            requestDateTime: new Date(pto.requestDateTime),
+            ptoStart: ptoStart,
+            ptoEnd: ptoEnd,
+          };
+        })
+      ),
     enabled: userId > 0,
   });
 };
 
-export default usePtoRequestsForSelectedYear
+export default usePtoRequestsForSelectedYear;
