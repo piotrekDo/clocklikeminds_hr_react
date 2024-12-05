3abae71b46e015347ad35bea894eb266319f8387
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2024-12-05 13:36:11 +0100

    server port updated

--- src/library.ts
+++ src/library.ts
@@ -21,7 +21,7 @@ export const darkElementBg = `#385898`;
 
 const BASE_URL_LOCALHOST = 'http://localhost:8080';
 const BASE_URL_AZURE = 'https://holidayapp.politewater-8bb1c971.westeurope.azurecontainerapps.io';
-const BASE_URL_VM = 'https://hr.cloud.clocklikeminds.com:8080';
+const BASE_URL_VM = 'https://hr.cloud.clocklikeminds.com:8443';
 export const BASE_URL = BASE_URL_VM;
 
 export const REACT_APP_REDIRECT_URL = BASE_URL + '/login/oauth2/code/google';
